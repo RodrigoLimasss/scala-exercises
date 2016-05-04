da8eda3916726d171c466a710ef45ab95c4420c5
Rodrigo Lima <rodrigolimasss@gmail.com>
2016-05-03 22:12:45 -0300

    Staircase.scala
    Add Staircase algorithm

---
+++ Staircase.scala
@@ -0,0 +1,14 @@
+/**
+  * Created by Rodrigo Lima on 03-May-16.
+  */
+  
+object Main extends App {
+  
+  def Staircase(n: Int) = {
+    (1 to n).foreach(x => {
+      println((" " * (n-x)) + ("#" * x))
+    })
+  }
+  Staircase(6)
+  
+}
