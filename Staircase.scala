6a3c28ba4341f2c7fc178b2abf89774b0d366524
Rodrigo Lima <rodrigolimasss@gmail.com>
2016-05-05 22:59:50 -0300

    Change App to Main

--- Staircase.scala
+++ Staircase.scala
@@ -1,23 +1,26 @@
 /**
   * Created by Rodrigo Lima on 03-May-16.
   */
-  
-object Main extends App {
-  
-  def Staircase(n: Int) = {
-    (1 to n).foreach(x => {
-      println((" " * (n-x)) + ("#" * x))
-    })
+
+object Solution {
+
+  def main(args: Array[String]) {
+
+    def Staircase(n: Int) = {
+      (1 to n).foreach(x => {
+        println((" " * (n - x)) + ("#" * x))
+      })
+    }
+    Staircase(6)
+
+    /*  output:
+              #
+             ##
+            ###
+           ####
+          #####
+         ######
+   */
+
   }
-  Staircase(6)
-  
-  /* output:
-        #
-       ##
-      ###
-     ####
-    #####
-   ######
- */
-  
 }
